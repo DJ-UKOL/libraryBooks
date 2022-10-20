30029d428a150232d17484e387a87a6e3df64547
DJ-UKOL <dj-ukol@yandex.ru>
2022-10-20 13:36:03 +0500

    Init project

--- src/main/java/org/example/model/Book.java
+++ src/main/java/org/example/model/Book.java
@@ -1,2 +1,37 @@
-package org.example.model;public class Book {
+package org.example.model;
+
+public class Book {
+    private String title;
+    private String author;
+    private int yearPublishing;
+
+    public Book(String title, String author, int yearPublishing) {
+        this.title = title;
+        this.author = author;
+        this.yearPublishing = yearPublishing;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublishing() {
+        return yearPublishing;
+    }
+
+    public void setYearPublishing(int yearPublishing) {
+        this.yearPublishing = yearPublishing;
+    }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,2 +1,32 @@
-package org.example.model;public class Person {
+package org.example.model;
+
+import jakarta.validation.constraints.Pattern;
+
+public class Person {
+    @Pattern(regexp = "[A-Z|А-Я]\\w+, [A-Z|А-Я]\\w+, [A-Z|А-Я]\\w+",
+            message = "Введите ФИО в правильном формате (Иванов Иван Иванович)")
+    private String name;
+    @Pattern(regexp = "[1,2][0|9][0-9][0-9]", message = "Год рождения должен быть в правильном формате (2022")
+    private int yearBirthday;
+
+    public Person(String name, int yearBirthday) {
+        this.name = name;
+        this.yearBirthday = yearBirthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearBirthday() {
+        return yearBirthday;
+    }
+
+    public void setYearBirthday(int yearBirthday) {
+        this.yearBirthday = yearBirthday;
+    }
 }
